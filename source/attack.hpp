74c8ed84f50dd44906ce088e654c0bbeaa2b2f43
wqt123 <643645865@qq.com>
2021-04-13 21:14:25 +0800

    修改灯条筛选

--- source/attack.hpp
+++ source/attack.hpp
@@ -222,9 +222,18 @@ class Attack : AttackBase {
         for (size_t i = 0; i < lights.size(); ++i) {
             for (size_t j = i + 1; j < lights.size(); ++j) {
                 cv::Point2f AC2BC = lights[j].centerPt - lights[i].centerPt;
+                /*对两个灯条的错位度进行筛选*/
+                float angle = (ligts[i].angle + lights[j].angle) / 2.0 / 180.0 * 3.14159265459;
+                if (abs(ligts[i].angle - ligts[j].angle) > 90) {
+                    angle += 3.14159265459 / 2;
+                }
+                cv::Vector2f orientation(cos(angle), sin(angle));
+                cv::Vector2f p2p(AC2BC.x,AC2BC.y);
+                if(abs(orientation.dot(p2p)) >= 25)
+                    continue
                 double minLength = cv::min(lights[i].length, lights[j].length);
                 double deltaAngle = cv::abs(lights[i].angle - lights[j].angle);
-                /* 对灯条组的长度，角度差，中心点tan值，x位置等进行筛选 */
+                /* 对灯条组的长度，角度差，中心点tan值，x位置等进行筛选， */
                 if ((deltaAngle > 23.0 && minLength < 20) || (deltaAngle > 11.0 && minLength >= 20) ||
                     cv::abs(lights[i].length - lights[j].length) / minLength > 0.5 ||
                     cv::fastAtan2(cv::abs(AC2BC.y), cv::abs(AC2BC.x)) > 25.0 ||
