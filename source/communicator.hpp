93ccd8bfb1e656d761950acd26dbdafe7597fc9e
wqt123 <643645865@qq.com>
2021-05-04 17:29:16 +0800

    反小陀螺

--- source/communicator.hpp
+++ source/communicator.hpp
@@ -51,12 +51,7 @@ class Communicator {
         float yaw = 0.0;
         float pitch = 0.0;
         float speed = 0.0;
-<<<<<<< HEAD
         float delay_time = 0.0;
-=======
-        unsigned char state = 0;
-        unsigned short time = 0;
->>>>>>> b34be6f7db263d62b9a52da6559af99254306825
         uint8_t extra[2] = {0, 0};  // additional imformation
         uint8_t crc8check = 0;
         uint8_t end = 0xf2;
